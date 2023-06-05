83093ec748151b525d28f57f844b20f27a0ec9c1
André Pedersen <andrped94@gmail.com>
2023-06-05 17:03:03 +0200

    Updated docker setup for deployment

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from neukit.gui import WebUI
 def main():
     print("Launching demo...")
 
-    cwd = "/Users/andreped/workspace/neukit/"  # local testing -> macOS
-    # cwd = "/home/user/app/"  # production -> docker
+    # cwd = "/Users/andreped/workspace/neukit/"  # local testing -> macOS
+    cwd = "/home/user/app/"  # production -> docker
 
     # initialize and run app
     app = WebUI(cwd=cwd)
