092699f35d265a34a0252554067a474cd0cfd6e4
André Pedersen <andrped94@gmail.com>
2023-06-06 12:21:13 +0200

    Added support for all preop brain tumor types

--- neukit/gui.py
+++ neukit/gui.py
@@ -4,7 +4,7 @@ from .inference import run_model
 
 
 class WebUI:
-    def __init__(self, model_name:str = None, class_name:str = "meningioma", cwd:str = "/home/user/app/"):
+    def __init__(self, model_name:str = None, cwd:str = "/home/user/app/"):
         # global states
         self.images = []
         self.pred_images = []
@@ -13,8 +13,24 @@ class WebUI:
         self.nb_slider_items = 150
 
         self.model_name = model_name
-        self.class_name = class_name
         self.cwd = cwd
+
+        self.class_name = "meningioma"  # default - but can be updated based on which task is chosen from dropdown
+        self.class_names = {
+            "meningioma": "MRI_Meningioma",
+            "low-grade": "MRI_LGGlioma",
+            "metastasis": "MRI_Metastasis",
+            "high-grade": "MRI_GBM",
+            "brain": "MRI_Brain",
+        }
+
+        self.result_names = {
+            "meningioma": "Tumor",
+            "low-grade": "Tumor",
+            "metastasis": "Tumor",
+            "high-grade": "Tumor",
+            "brain": "Brain",
+        }
 
         # define widgets not to be rendered immediantly, but later on
         self.slider = gr.Slider(1, self.nb_slider_items, value=1, step=1, label="Which 2D slice to show")
@@ -24,6 +40,10 @@ class WebUI:
             visible=True,
             elem_id="model-3d",
         ).style(height=512)
+    
+    def set_class_name(self, value):
+        print("Changed task to:", value)
+        self.class_name = value
 
     def combine_ct_and_seg(self, img, pred):
         return (img, [(pred, self.class_name)])
@@ -33,7 +53,7 @@ class WebUI:
     
     def load_mesh(self, mesh_file_name):
         path = mesh_file_name.name
-        run_model(path, model_path=self.cwd + "resources/models/")
+        run_model(path, model_path=self.cwd + "resources/models/", task=self.class_names[self.class_name], name=self.result_names[self.class_name])
         nifti_to_glb("prediction.nii.gz")
 
         self.images = load_ct_to_numpy(path)
@@ -56,25 +76,42 @@ class WebUI:
         height: 512px;
         margin: auto;
         }
+        #upload {
+        height: 120px;
+        }
         """
         with gr.Blocks(css=css) as demo:
 
             with gr.Row():
-                file_output = gr.File(
-                    file_count="single"
-                ).style(full_width=False, size="sm")
+                
+                file_output = gr.File(file_count="single", elem_id="upload")  # elem_id="upload"
                 file_output.upload(self.upload_file, file_output, file_output)
 
-                run_btn = gr.Button("Run analysis").style(full_width=False, size="sm")
+                # with gr.Column():
+
+                model_selector = gr.Dropdown(
+                    list(self.class_names.keys()),
+                    label="Task",
+                    info="Which task to perform - one model for each brain tumor type and brain extraction",
+                    multiselect=False,
+                    size="sm",
+                )
+                model_selector.input(
+                    fn=lambda x: self.set_class_name(x),
+                    inputs=model_selector,
+                    outputs=None,
+                )
+
+                run_btn = gr.Button("Run analysis").style(full_width=False, size="lg")
                 run_btn.click(
                     fn=lambda x: self.load_mesh(x),
                     inputs=file_output,
-                    outputs=self.volume_renderer
+                    outputs=self.volume_renderer,
                 )
             
             with gr.Row():
                 gr.Examples(
-                    examples=[self.cwd + "RegLib_C01_2.nii"],
+                    examples=[self.cwd + "RegLib_C01_1.nii", self.cwd + "RegLib_C01_2.nii"],
                     inputs=file_output,
                     outputs=file_output,
                     fn=self.upload_file,
